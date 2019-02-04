98a20e31eef241358ed46f7c6a747459929946d1
Lars-Martin <lars.m.antonsen@gmail.com>
2019-02-04 17:51:56 +0100

    nytt prosjekt route

--- v1/routes/prosjekter.js
+++ v1/routes/prosjekter.js
@@ -7,4 +7,9 @@ router.get("/",function(req,res){
     res.render("../views/prosjekter/index.ejs");
 });
 
+// Nytt prosjekt - Legger et nytt prosjekt til db
+router.get("/new", function(req,res){
+    res.render("prosjekter/new");
+});
+
 module.exports = router;
