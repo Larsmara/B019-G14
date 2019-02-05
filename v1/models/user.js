734c6c3394e1ef7768ab00d9a45f898568cb2ec6
Larsmara <larsmara@hiof.no>
2019-02-05 14:12:40 +0100

    Navigering.
    Navbar routes. Startet på bruker objekt og logikk for å håndtere sign up.

--- v1/models/user.js
+++ v1/models/user.js
@@ -2,7 +2,10 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-
+    navn: String,
+    email: String, 
+    tlf: String,
+    passord: String
 });
 
 UserSchema.plugin(passportLocalMongoose);
--- v1/routes/index.js
+++ v1/routes/index.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
 
 
 // ===============================
@@ -10,10 +12,24 @@ var router = express.Router();
 router.get("/register", function(req,res){
     res.render("register");
 });
+// Logikk for registrering
+applicationCache.use("/register", function(req,res){
+    var newUser = new User({});
+})
 
 // Login skjema
 router.get("/login",function(req,res){
     res.render("login");
+});
+
+// Min side skjema
+router.get("/mySite", function(req,res){
+    res.render("mySite");
+});
+
+// Om oss skjema
+router.get("/about", function(req,res){
+    res.render("about");
 });
 
 //Root route
