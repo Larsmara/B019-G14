9baeefe97b3d5ce105019b3fdba7aaa216050555
Lars-Martin <lars.m.antonsen@gmail.com>
2019-02-22 17:28:29 +0100

    5

---
+++ v2/src/components/project/ProjectShow.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="project-show">
+        <h2>Viser et spesifikt prosjekt</h2>
+    </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+import firebase from 'firebase'
+
+export default {
+    name: 'ProjectShow',
+    data(){
+        return {
+            project: null
+        }
+    },
+    created(){
+        // comments
+        db.collection('projects').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                console.log(doc.data())
+            })
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
