720e1f39f78525bb376ed6c81bc9c01aee2adc99
Larsmara <larsmara@hiof.no>
2019-02-23 01:12:41 +0100

    projectshow

--- v2/src/components/project/ProjectShow.vue
+++ v2/src/components/project/ProjectShow.vue
@@ -1,6 +1,11 @@
 <template>
-    <div class="project-show">
+    <div class="project-show container">
         <h2>Viser et spesifikt prosjekt</h2>
+        <div v-for="pro in project" :key="pro.id">
+            <h3>{{pro.title}}</h3>
+            <p>{{pro.content}}</p>
+            <p>{{pro.time}}</p>
+        </div>
     </div>
 </template>
 
@@ -8,17 +13,38 @@
 import db from '@/firebase/init'
 import firebase from 'firebase'
 import Project from './Project'
+import Projects from './Project'
+import moment from 'moment'
 
 export default {
+    props: ['id'],
     name: 'ProjectShow',
+    components: {Project, Projects},
     data(){
         return {
-            project: null
+            project: []
         }
     },
     created(){
-        // comments
-        console.log($route.params.id)
+        console.log(this.$route.params.id)
+        let project = db.collection("projects")
+        console.log(project)
+
+        project.where('title', '==', this.$route.params.id)
+        .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type == 'added'){
+                    let doc = change.doc
+                    this.project.push({
+                        title: doc.data().title,
+                        content: doc.data().content,
+                        time: moment(doc.data().timestamp).format('lll')
+                    })
+                    console.log(change.doc.data())
+                }
+            })
+        })
+        
     }
 }
 </script>
